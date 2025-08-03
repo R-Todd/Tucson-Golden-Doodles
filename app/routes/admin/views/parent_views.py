d4e39e4d9b712061a60c085cdabf641c1fa4f0ac
Ryan Todd <r2neatha678@gmail.com>
2025-08-03 11:00:20 -0700

    Restore image upload handling in ParentAdminView
    Re-enabled the on_model_change method to handle image uploads for parent records. Also improved edit_form to add 'data-current-image' attributes for image preview functionality.

--- app/routes/admin/views/parent_views.py
+++ app/routes/admin/views/parent_views.py
@@ -14,41 +14,47 @@ class ParentAdminView(AdminModelView):
     def edit_form(self, obj=None):
         form = super(ParentAdminView, self).edit_form(obj)
         
-        # --- DEBUGGING ---
-        print("\n" + "="*50)
-        print("DEBUG (Simplified View): Inside ParentAdminView.edit_form")
-        if form:
-            print(f"Form object type: {type(form)}")
-            print(f"Fields available: {list(form._fields.keys())}")
-        print("="*50 + "\n")
-        # --- END DEBUGGING ---
-
+        # This logic adds the 'data-current-image' attribute for the previews
+        if obj:
+            image_fields = {
+                'image_upload': obj.main_image_url,
+                'alternate_image_upload_1': obj.alternate_image_url_1,
+                'alternate_image_upload_2': obj.alternate_image_url_2,
+                'alternate_image_upload_3': obj.alternate_image_url_3,
+                'alternate_image_upload_4': obj.alternate_image_url_4,
+            }
+            for field_name, image_url in image_fields.items():
+                if hasattr(form, field_name) and image_url:
+                    field = getattr(form, field_name)
+                    if field.render_kw is None:
+                        field.render_kw = {}
+                    field.render_kw['data-current-image'] = image_url
         return form
 
-    # --- TEMPORARILY DISABLED FOR DEBUGGING ---
-    # def on_model_change(self, form, model, is_created):
-    #     """Handle image uploads when a parent record is saved."""
-    #     main_file = request.files.get('image_upload')
-    #     if main_file and main_file.filename:
-    #         image_urls = upload_image(main_file, folder='parents', create_responsive_versions=True)
-    #         if image_urls:
-    #             model.main_image_url = image_urls.get('original')
-    #             model.main_image_url_small = image_urls.get('small')
-    #             model.main_image_url_medium = image_urls.get('medium')
-    #             model.main_image_url_large = image_urls.get('large')
-    #
-    #     alt_fields = [
-    #         'alternate_image_upload_1', 'alternate_image_upload_2',
-    #         'alternate_image_upload_3', 'alternate_image_upload_4'
-    #     ]
-    #     alt_model_attrs = [
-    #         'alternate_image_url_1', 'alternate_image_url_2',
-    #         'alternate_image_url_3', 'alternate_image_url_4'
-    #     ]
-    #
-    #     for i, field_name in enumerate(alt_fields):
-    #         file = request.files.get(field_name)
-    #         if file and file.filename:
-    #             url = upload_image(file, folder='parents_alternates')
-    #             if url:
-    #                 setattr(model, alt_model_attrs[i], url)
+    # --- THIS METHOD IS NOW RESTORED ---
+    def on_model_change(self, form, model, is_created):
+        """Handle image uploads when a parent record is saved."""
+        main_file = request.files.get('image_upload')
+        if main_file and main_file.filename:
+            image_urls = upload_image(main_file, folder='parents', create_responsive_versions=True)
+            if image_urls:
+                model.main_image_url = image_urls.get('original')
+                model.main_image_url_small = image_urls.get('small')
+                model.main_image_url_medium = image_urls.get('medium')
+                model.main_image_url_large = image_urls.get('large')
+
+        alt_fields = [
+            'alternate_image_upload_1', 'alternate_image_upload_2',
+            'alternate_image_upload_3', 'alternate_image_upload_4'
+        ]
+        alt_model_attrs = [
+            'alternate_image_url_1', 'alternate_image_url_2',
+            'alternate_image_url_3', 'alternate_image_url_4'
+        ]
+
+        for i, field_name in enumerate(alt_fields):
+            file = request.files.get(field_name)
+            if file and file.filename:
+                url = upload_image(file, folder='parents_alternates')
+                if url:
+                    setattr(model, alt_model_attrs[i], url)
