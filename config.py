928d94425c89393e49744e0938b8aa939b916932
Ryan Todd <r2neatha678@gmail.com>
2025-09-02 14:06:22 -0700

    Enhance admin template and config comments
    Added more detailed comments and structure to admin/base_bs5.html for clarity on included stylesheets and scripts. Improved config.py with clearer sectioning and comments for configuration variables, and replaced hardcoded SECRET_KEY fallback with an error if not set.

--- config.py
+++ config.py
@@ -6,18 +6,22 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     """Set Flask configuration variables from environment variables."""
+    # Flask-related configurations
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    # Throw error if the secret key is not found
+    # Throw error if the secret key is not found - Replaces hardcoded backup key
     if not SECRET_KEY:
         raise ValueError("No Secret_Key set for Flask application.")
     
+    # SQLAlchemy database configurations
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
     """Configuration for testing."""
+    # Enable testing mode
     TESTING = True
+    # Use an in-memory SQLite database for tests
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use in-memory SQLite database
     WTF_CSRF_ENABLED = False  # Disable CSRF forms in tests for simplicity
     SECRET_KEY = 'test-secret-key'
